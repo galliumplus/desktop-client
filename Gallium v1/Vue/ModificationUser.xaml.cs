f3bd6ef50731806b6f671382ba5f7f94c19ad756
Damien <Damien@DESKTOP-SF82095>
2022-06-15 17:59:25 +0200

    Modification User - Amélioration IHM et Biding en cours

--- Gallium v1/Vue/ModificationUser.xaml.cs
+++ Gallium v1/Vue/ModificationUser.xaml.cs
@@ -20,20 +20,38 @@ namespace Gallium_v1.Vue
     /// </summary>
     public partial class ModificationUser : Window
     {
-        User user;
+        private User user;
+
+        private string acompte;
+        private string nom;
+        private string prenom;
+        private double balance;
+        private string mdp;
+
 
         public ModificationUser(User u)
         {
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
+            this.user = u;
+            this.nom = this.user.Nom.Split(' ')[0];
+            this.prenom = this.user.Nom.Split(' ')[1];
+            this.acompte = this.user.Compte;
+            this.balance = this.user.Balance;
             
         }
 
+        /// <summary>
+        /// Permet de valider les modifications faite
+        /// </summary>
         private void ValiderModif(object sender, RoutedEventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Permet d'annuler les modifications
+        /// </summary>
         private void AnnulerModif(object sender, RoutedEventArgs e)
         {
             this.Close();
