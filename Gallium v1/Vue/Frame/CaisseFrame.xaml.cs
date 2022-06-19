3b73e8378473e5e5aa661bc8f9aaed38fdb6961b
matteobadet <matteo.badet@iut-dijon.u-bourgogne.fr>
2022-06-19 09:31:36 +0200

    Caisse Update (btn payer)

--- Gallium v1/Vue/Frame/CaisseFrame.xaml.cs
+++ Gallium v1/Vue/Frame/CaisseFrame.xaml.cs
@@ -41,6 +41,43 @@ namespace Gallium_v1.Vue.Frame
             Grid gd = sender as Grid;
             gd.Background = Brushes.Transparent;
         }
+        private void OrderItem_Enter(object sender, MouseEventArgs e)
+        {
+            Grid gd = sender as Grid;
+            gd.Background = Brushes.Gray;
+        }
+
+        private void OrderItem_Leave(object sender, MouseEventArgs e)
+        {
+            Grid gd = sender as Grid;
+            gd.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#302F2F");
+        }
+        private void Buy_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Grid gd = sender as Grid;
+            gd.Background = Brushes.Gray;
+        }
+
+        private void Buy_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Grid gd = sender as Grid;
+            gd.Background = Brushes.Black;
+        }
+
+        private void Drop_Item_Order(object sender, MouseEventArgs e)
+        {
+            Grid gd = sender as Grid;
+
+            for(int i = 0; i < orderedItem.Count; i++)
+            {
+                if (orderedItem[i].NomProduit == (string)gd.Tag)
+                {
+                    orderedItem.RemoveAt(i);
+                }
+            }
+
+            UpdateListProduitsOrder();
+        }
 
         private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
