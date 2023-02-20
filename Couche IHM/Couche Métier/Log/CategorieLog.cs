4405ae838fc6ce0238eab1ba0cfe1f1c13c47b99
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-02-20 16:57:15 +0100

    Ajout fichier log

---
+++ Couche IHM/Couche Métier/Log/CategorieLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couche_Métier.Log
+{
+    public enum CategorieLog
+    {
+        // GENERAL
+        CONNEXION,
+        VENTE,
+        // PRODUCTS
+        ADD_PRODUCT,
+        DELETE_PRODUCT,
+        UPDATE_PRODUCT,
+        // ADHERENTS
+        CREATE_ADHERENT,
+        DELETE_ADHERENT,
+        UPDATE_ADHERENT,
+        // USER
+        CREATE_USER,
+        DELETE_USER,
+        UPDATE_USER
+
+    }
+}
---
+++ Couche IHM/Couche Métier/Log/LogToTxt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couche_Métier.Log
+{
+    /// <summary>
+    /// Enregistre des logs dans un fichier dont l'extension est .txt
+    /// </summary>
+    public class LogToTXT : ILog
+    {
+        public string Path => @Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Gallium\\Log\\GalliumLog.txt";
+
+        public void registerLog(CategorieLog categorieLog, string message)
+        {
+            // Créer le fichier s'il n'existe pas
+            if (!File.Exists(Path))
+            {
+                File.Create(Path);
+            }
+
+            // Sauvegarde le log
+            message = $"{DateTime.Now} | {categorieLog.ToString()} | {message}";
+            using (StreamWriter file = new(Path, append: true))
+            {
+                file.WriteLine(message);
+            }
+        }
+
+        public List<string> loadLog()
+        {
+            return null;
+        }
+
+    }
+}
