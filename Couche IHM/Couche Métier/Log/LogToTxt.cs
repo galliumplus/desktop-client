9229ddd9243ce0935cf4e9b3e15bb7d520af3438
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-02-20 20:02:32 +0100

    Correction bug fichier

--- Couche IHM/Couche Métier/Log/LogToTxt.cs
+++ Couche IHM/Couche Métier/Log/LogToTxt.cs
@@ -11,20 +11,25 @@ namespace Couche_Métier.Log
     /// </summary>
     public class LogToTXT : ILog
     {
-        public string Path => @Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Gallium\\Log\\GalliumLog.txt";
+        public string Path => @Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Gallium\\Log";
+        private string name => "\\GalliumLog.txt";
 
         public void registerLog(CategorieLog categorieLog, string message)
         {
-            // Créer le fichier s'il n'existe pas
-            if (!File.Exists(Path))
+            // Gestion fichiers
+            if (!Directory.Exists(Path)) // Créer le dossier s'il n'existe pas
             {
-                Directory.CreateDirectory(Path.Split('\\')[Path.Split('\\').Length - 1]);
-                File.Create(Path);
+                Directory.CreateDirectory(Path);
+                if(!File.Exists(Path+name)) // Créer le fichier si n'existe pas
+                {
+                    File.Create(Path+name);
+                }
             }
+            
 
             // Sauvegarde le log
-            message = $"{DateTime.Now} | {categorieLog.ToString()} | {message}";
-            using (StreamWriter file = new(Path, append: true))
+            message = $"{DateTime.Now} | {categorieLog} | {message}";
+            using (StreamWriter file = new(Path+name, append: true))
             {
                 file.WriteLine(message);
             }
