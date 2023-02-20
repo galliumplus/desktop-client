c39f7909d9d9c4257947136bd19eb608c988f3c6
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-02-20 16:55:24 +0100

    Ajout ILOg

---
+++ Couche IHM/Couche Métier/Log/ILog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couche_Métier.Log
+{
+    public interface ILog
+    {
+        /// <summary>
+        /// Path du fichier
+        /// </summary>
+        public string Path { get; }
+
+        /// <summary>
+        /// Enregistrer les logs
+        /// </summary>
+        /// <param name="categorieLog"> Categorie du log </param>
+        /// <param name="message"> message à enregistrer </param>
+        public void registerLog(CategorieLog categorieLog, string message);
+
+        /// <summary>
+        /// Récupère les logs
+        /// </summary>
+        public List<string> loadLog();
+    }
+}
