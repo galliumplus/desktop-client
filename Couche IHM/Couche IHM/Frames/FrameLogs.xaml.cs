19377f1e3628ee6152c86e05db99e44df608ebc6
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-02-22 09:32:39 +0100

    Précision des logs

--- Couche IHM/Couche IHM/Frames/FrameLogs.xaml.cs
+++ Couche IHM/Couche IHM/Frames/FrameLogs.xaml.cs
@@ -37,20 +37,54 @@ namespace Couche_IHM.Frames
         private void FillListView()
         {
             ILog log = new LogToTXT();
-            
-            List<string> logsLine = new List<string>();
+            List<string> logsLine = log.loadLog();
             List<Log> list = new List<Log>();
-            for(int i = logsLine.Count; i > 0; i--)
+            for(int i = logsLine.Count - 1; i > 0; i--)
             {
                 string date = logsLine[i].Split('|')[0];
                 string action = logsLine[i].Split('|')[1];
                 string message = logsLine[i].Split('|')[2];
                 string auteur = logsLine[i].Split('|')[3];
                 list.Add(new Log(date, action, message, auteur));
+
+
+                // TEST =================================================================================
+                if(montYear != DateTime.Parse(date).ToString("MMMM yyyy"))
+                {
+                    montYear = DateTime.Parse(date).ToString("MMMM yyyy");
+                    listMonthYear.Add(montYear);
+                }
+                // ========================================================================================
             }
-            foreach(string logItem in log.loadLog())
+            this.listLogs.ItemsSource = list;
+        }
+
+        /// <summary>
+        /// Créer enumération 
+        /// </summary>
+        /// <param name="list"> Ce que contient l'enum </param>
+        /// <returns> renvoie l'enumération </returns>
+        private Array createEnum(List<string> list)
+        {
+            // Get the current application domain for the current thread
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+
+            // Create a dynamic assembly in the current application domain,
+            // and allow it to be executed and saved to disk.
+            AssemblyName name = new AssemblyName("MyEnums");
+            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(Guid.NewGuid().ToString()),AssemblyBuilderAccess.Run);
+
+            // Define a dynamic module in "MyEnums" assembly.
+            // For a single-module assembly, the module has the same name as the assembly.
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(name.Name);
+
+            // Define a public enumeration with the name "MyEnum" and an underlying type of Integer.
+            EnumBuilder myEnum = moduleBuilder.DefineEnum("EnumeratedTypes.MyEnum",
+                                     TypeAttributes.Public, typeof(int));
+
+            for(int i = 0; i < list.Count; i++)
             {
-                
+                myEnum.DefineLiteral(list[i], i);
             }
             this.listLogs.ItemsSource = list;
         }
