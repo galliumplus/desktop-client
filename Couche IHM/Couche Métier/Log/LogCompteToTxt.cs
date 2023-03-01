e3594d85980bbd716ca8e51c5a8b82100744f7f9
Florian <florian.marteau@iut-dijon.u-bourgogne.fr>
2023-03-01 09:03:21 +0100

    ajout fichiers

---
+++ Couche IHM/Couche Métier/Log/LogCompteToTxt.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couche_Métier.Log
+{
+    /// <summary>
+    /// Enregistre des logs dans un fichier dont l'extension est .txt
+    /// </summary>
+    public class LogCompteToTxt : ILog
+    {
+        public string Path => @Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Gallium\\Log";
+        private string name = "\\GalliumLog.txt";
+
+        public void registerLog(CategorieLog categorieLog, object itemModif, User author)
+        {
+            // Vérification du fichier et récupération de la modification
+            VerifyFiles();
+            User u = (User)itemModif;
+
+            // Modification du message selon l'opération
+            string message = "";
+            switch (categorieLog)
+            {
+                case CategorieLog.UPDATE:
+                    
+                    break;
+                case CategorieLog.CREATE:
+                    message = $"Le compte {u.NomComplet} a été créé";
+                    break;
+                case CategorieLog.DELETE:
+                    message = $"Le compte {u.NomComplet} a été supprimé";
+                    break;
+
+            }      
+            string log = $"{DateTime.Now}|COMPTE|{message}|{author.NomComplet}|{categorieLog}";
+
+            // Ecriture du log
+            using (StreamWriter file = new(Path+name, append: true))
+            {
+                file.WriteLine(log);
+            }
+        }
+
+        /// <summary>
+        /// Récupère les logs
+        /// </summary>
+        /// <returns></returns>
+        public List<string> loadLog()
+        {
+            VerifyFiles();
+            List<string> logs = new List<string>();
+            foreach (string line in System.IO.File.ReadLines(Path+name))
+            {
+                logs.Add(line);
+            }
+            return logs;
+        }
+
+        /// <summary>
+        /// Vérifie la bonne existences des fichiers
+        /// </summary>
+        private void VerifyFiles()
+        {
+            // Gestion Dossier
+            if (!Directory.Exists(Path)) // Créer le dossier s'il n'existe pas
+            {
+                Directory.CreateDirectory(Path);
+            }
+
+            // Création fichier
+            if (!File.Exists(Path + name)) // Créer le fichier si n'existe pas
+            {
+                FileStream stream = File.Create(Path + name);
+                stream.Close();
+            }
+        }
+    }
+}
---
+++ Couche IHM/Couche Métier/Log/LogProductToTxt.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couche_Métier.Log
+{
+    public class LogProductToTxt : ILog
+    {
+        public string Path => @Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Gallium\\Log";
+        private string name = "\\GalliumLog.txt";
+
+        public List<string> loadLog()
+        {
+            VerifyFiles();
+            List<string> logs = new List<string>();
+            foreach (string line in System.IO.File.ReadLines(Path + name))
+            {
+                logs.Add(line);
+            }
+            return logs;
+        }
+
+        public void registerLog(CategorieLog categorieLog, object itemModif, User author)
+        {
+            // Vérification du fichier et récupération de la modification
+            VerifyFiles();
+            Product p = (Product)itemModif;
+
+            // Modification du message selon l'opération
+            string message = "";
+            switch (categorieLog)
+            {
+                case CategorieLog.UPDATE:
+
+                    break;
+                case CategorieLog.CREATE:
+                    message = $"Le produit {p.NomProduit} a été créé";
+                    break;
+                case CategorieLog.DELETE:
+                    message = $"Le produit {p.NomProduit} a été supprimé";
+                    break;
+
+            }
+            string log = $"{DateTime.Now}|PRODUIT|{message}|{author.NomComplet}|{categorieLog}";
+
+            // Ecriture du log
+            using (StreamWriter file = new(Path + name, append: true))
+            {
+                file.WriteLine(log);
+            }
+        }
+
+        /// <summary>
+        /// Vérifie la bonne existences des fichiers
+        /// </summary>
+        private void VerifyFiles()
+        {
+            // Gestion Dossier
+            if (!Directory.Exists(Path)) // Créer le dossier s'il n'existe pas
+            {
+                Directory.CreateDirectory(Path);
+            }
+
+            // Création fichier
+            if (!File.Exists(Path + name)) // Créer le fichier si n'existe pas
+            {
+                FileStream stream = File.Create(Path + name);
+                stream.Close();
+            }
+        }
+    }
+}
