d083103c044daa9f4cf3db3fcf296a22b25a47c1
Damien CHABRET <damien.chabret@iut-dijon.u-bourgogne.fr>
2023-03-02 08:06:40 +0100

    Changement FenetreAddUser
    Changement User

--- Couche IHM/Couche IHM/Frames/FenetreAddUser.xaml.cs
+++ Couche IHM/Couche IHM/Frames/FenetreAddUser.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Couche_Métier;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,11 +20,92 @@ namespace Couche_IHM.Frames
     /// </summary>
     public partial class FenetreAddUser : Window
     {
-        public FenetreAddUser()
+        private User copyUser;
+
+        public FenetreAddUser(User copyUser)
         {
             InitializeComponent();
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.copyUser = copyUser;
 
+        }
+
+        /// <summary>
+        /// Vérifie que le formulaire n'est pas vide
+        /// </summary>
+        /// <returns> true si il n'est pas vide, false si il est vide</returns>
+        private bool IsUserNotNull()
+        {
+            bool notnull = true;
+
+            // Identifiant
+            if (string.IsNullOrEmpty(identifiantUser.Text))
+            {
+                notnull = false;
+            }
+
+            // Nom
+            if (string.IsNullOrEmpty(nomUser.Text))
+            {
+                notnull = false;
+            }
+
+            // Prénom
+            if (string.IsNullOrEmpty(prenomUser.Text))
+            {
+                notnull = false;
+            }
+
+            // Mot de passe
+            if (string.IsNullOrEmpty(passwordUser.Text))
+            {
+                notnull = false;
+            }
+
+            // Mot de passe2
+            if (string.IsNullOrEmpty(this.password2User.Text))
+            {
+                notnull = false;
+            }
+
+            // Rôle
+            if (this.roleUser.Text is null)
+            {
+                notnull = false;
+            }
+            
+
+            return notnull;
+        }
+
+        /// <summary>
+        /// Empêche l'utilisateur d'entrer un espace
+        /// </summary>
+        private void isSpaceBox(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Valide les changements
+        /// </summary>
+        private void ValideChange(object sender, RoutedEventArgs e)
+        {
+            if (IsUserNotNull())
+            {
+                this.DialogResult = true;
+            }
+        }
+
+        /// <summary>
+        /// Annule les changemements
+        /// </summary>
+        private void CancelChange(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
         }
     }
 }
