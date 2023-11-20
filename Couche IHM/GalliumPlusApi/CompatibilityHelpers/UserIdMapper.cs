cf80f53301d1b879a33eea6b556424bfc1903a96
Fatéo Mavard <94078903+IndorieSama@users.noreply.github.com>
2023-11-20 07:40:59 +0100

    ouuuuuu

---
+++ Couche IHM/GalliumPlusApi/CompatibilityHelpers/UserIdMapper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalliumPlusApi.CompatibilityHelpers
+{
+    internal class UserIdMapper
+    {
+        private static UserIdMapper? current;
+
+        public static UserIdMapper Current
+        {
+            get
+            {
+                if (current == null)
+                {
+                    current = new UserIdMapper();
+                }
+                return current;
+            }
+        }
+
+        private Dictionary<string, int> ids;
+
+        private UserIdMapper()
+        {
+            ids = new Dictionary<string, int>();
+        }
+
+        public int GetIdFor(string username)
+        {
+            lock (ids)
+            {
+                if (ids.ContainsKey(username))
+                {
+                    return ids[username];
+                }
+                else
+                {
+                    Span<byte> binaryData = new();
+                    Base64.DecodeFromUtf8(Encoding.UTF8.GetBytes(username), binaryData, out _, out _);
+                    int id = BitConverter.ToInt32(binaryData);
+                    ids[username] = id;
+                    return id;
+                }
+            }
+        }
+
+        public string FindUsernameOf(int numericId)
+        {
+            lock (ids)
+            {
+                return ids.First(kvp => kvp.Value == numericId).Key;
+            }
+        }
+    }
+}
---
+++ Couche IHM/GalliumPlusApi/Exceptions/CantSellException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalliumPlusApi.Exceptions
+{
+    internal class CantSellException : GalliumPlusHttpException
+    {
+        public CantSellException() : base("Impossible de passer la commande") { }
+
+        public CantSellException(string message) : base(message) { }
+    }
+}
